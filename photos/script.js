759a4dfe3e562fcbea03984db0eb311bedcd3632
KhanyaI <=>
2025-07-25 16:59:47 +0200

    photo edits

--- photos/script.js
+++ photos/script.js
@@ -1,6 +1,6 @@
 // Sample images - replace these with your own photo paths
 const images = [
-    "https://khanyai.github.io/photos/analog1.jpg?w=800&h=600&fit=crop",
+    "analog1.jpg?w=800&h=600&fit=crop",
     "https://khanyai.github.io/photos/analog2.jpg?w=800&h=600&fit=crop",
     "https://khanyai.github.io/photos/digital1.jpg?w=800&h=600&fit=crop",
     "https://khanyai.github.io/photos/digital2.jpg?w=800&h=600&fit=crop",
